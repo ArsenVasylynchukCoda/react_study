4a824cdf6885fb29982a369730dbf7da409db239
Arsen <arsenvasylynchuk@gmail.com>
2023-11-12 17:23:10 +0100

    First routes test

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import './App.css';
 import Users from './components/Users/Users';
 import Form from './components/Form/Form';
 import Todo from './components/Todo/Todo';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <Todo />
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path="/users" element={<Users />} />
+        <Route path='/todo' element={<Todo/>} />
+        {/* <Route path="/users" element={<UsersPage />}>
+          <Route path="/users/:id" element={<User />} />
+        </Route> */}
+        {/* <Route path="/not-found" element={<NotFoundPage />} /> */}
+        {/* <Route path="*" element={<Navigate to="/not-found" />} /> */}
+      </Routes>
+    </div >
   );
 }
 
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+function HomePage () {
+    return (
+        <h1>My first project!</h1>
+    )
+}
+
+export default HomePage
